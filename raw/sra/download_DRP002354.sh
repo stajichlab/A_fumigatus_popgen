942bc664be9e78bb540d09c9ed6e3fb76f87aa39
Jason Stajich <jason@bioperl.org>
2015-05-30 23:09:12 -0700

    master download by SRA

---
+++ raw/sra/download_DRP002354.sh
@@ -0,0 +1,3 @@
+#PBS -l nodes=1:ppn=1,mem=2gb,walltime=2:00:00 -N aspergillusSRA -j oe 
+module load aspera
+ascp -i /opt/aspera/3.3.3/etc/asperaweb_id_dsa.openssh -k2 -T -l200m anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByStudy/sra/DRP/DRP002/DRP002354/ ./
